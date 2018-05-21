eab88d0efc3eed7b670cdf82a6ffc1f76e78d462
Pengzhi Sun <pengzhisun@live.com>
2018-05-22 04:27:20 +0800

    init project stub

--- demos/console_demo/Demos/IDemo.cs
+++ demos/console_demo/Demos/IDemo.cs
@@ -1,8 +1,9 @@
 //------------------------------------------------------------------------------
-// <copyright file="IDemo.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// <copyright file="IDemo.cs" company="Pengzhi Sun">
+// Copyright (c) Pengzhi Sun. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
-//------------------------------------------------------------------------------
+// -----------------------------------------------------------------------
 
 namespace Winl.AzureDevBox.ConsoleDemo.Demos
 {
